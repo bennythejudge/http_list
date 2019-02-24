3d5aa17a30ae3fd88baab6fdc7f4c6171aefaf55
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2019-02-25 00:42:50 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(new MaterialApp(
+  home: new HomePage(),
+));
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => new HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Retrieve JSON via HTTP Get")
+      ),
+      body: new ListView.builder(
+        itemCount: 1,
+        itemBuilder: (BuildContext context, int index) {
+          return new Container(
+            child: new Center(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  new Card(
+                    child: new Container(
+                      child: new Text("Hello"),
+                      padding: const EdgeInsets.all(20.0)),
+                    )
+                ]
+              )
+            )
+          );
+        },
+      )
+    );
+  }
+}
